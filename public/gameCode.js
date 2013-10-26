f87e0e3a713253b3ce2472ad399c7a4f6c2454cb
Alex Hawker <pumpkinpi64@gmail.com>
2013-10-26 01:09:55 -0700

    Bump mapping, here we go!

--- public/gameCode.js
+++ public/gameCode.js
@@ -26,10 +26,14 @@ var glProgram; //program holding the shaders
 var vertexPositionAttribute; //pointer to vertexPosition in the shader
 var vertexNormalAttribute; //pointer to vertexNormal in the shader
 var vertexTextureAttribute; //pointer to vertexTexture in the shader
+var vertexTangentAttribute;
 
 var pUniform; //perspective matrix
 var nUniform; //normal matrix
 var mvUniform; //model-view matrix
+
+var samplerUniform;
+var normalMapUniform;
 
 var lightNum=2;
 var lightPositionUniform=[];
@@ -464,24 +468,22 @@ function initShaders() {
   vertexTextureAttribute = gl.getAttribLocation(glProgram, "vertexTexture");
   gl.enableVertexAttribArray(vertexTextureAttribute);
   
+  vertexTangentAttribute = gl.getAttribLocation(glProgram, "vertexTangent");
+  gl.enableVertexAttribArray(vertexTangentAttribute);
+
   pUniform = gl.getUniformLocation(glProgram, "uPMatrix"); //perspective matrix
   nUniform = gl.getUniformLocation(glProgram, "normalMatrix"); //normal matrix
   mvUniform = gl.getUniformLocation(glProgram, "uMVMatrix"); //model-view matrix
   
   lightPositionUniform[0] = gl.getUniformLocation(glProgram, "lightPosition");
-  lightPositionUniform[1] = gl.getUniformLocation(glProgram, "lightPosition2");
-  lightPositionUniform[2] = gl.getUniformLocation(glProgram, "lightPosition3");
   lightMaterialUniform[0] = gl.getUniformLocation(glProgram, "lightMaterial");
-  lightMaterialUniform[1] = gl.getUniformLocation(glProgram, "lightMaterial2");
-  lightMaterialUniform[2] = gl.getUniformLocation(glProgram, "lightMaterial3");
   lightDistanceUniform[0] = gl.getUniformLocation(glProgram, "lightDistance");
-  lightDistanceUniform[1] = gl.getUniformLocation(glProgram, "lightDistance2");
   gl.uniform1f(lightDistanceUniform[0], LIGHT_DIST[0]);
   gl.uniform1f(lightDistanceUniform[1], LIGHT_DIST[1]);
   gl.uniform3f(lightMaterialUniform[0], lightMaterial[0], lightMaterial[1], lightMaterial[2]); //player Light
   
-  lightingDisabledUniform = gl.getUniformLocation(glProgram, "lightingDisabled");
-  texCoordAddUniform = gl.getUniformLocation(glProgram, "texCoordAdd");
+  samplerUniform = gl.getUniformLocation(glProgram, "sampler");
+  normalMapUniform = gl.getUniformLocation(glProgram, "normalMap");
 }
 
 /* Code From:
@@ -635,11 +637,18 @@ function drawScene() {
   
     gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer[playerObj]);
     gl.vertexAttribPointer(vertexTextureAttribute, 2, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, tangentBuffer[playerObj]);
+    gl.vertexAttribPointer(vertexTangentAttribute, 4, gl.FLOAT, false, 0, 0);
   
   
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, texture[playerTexture]);
-    gl.uniform1i(gl.getUniformLocation(glProgram, "sampler"), 0);
+    gl.uniform1i(samplerUniform, 0);
+
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, texture[1]);
+    gl.uniform1i(normalMapUniform, 1);
   
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer[playerObj]);
     setNormalMatrix();
@@ -658,7 +667,11 @@ function drawScene() {
      
   gl.activeTexture(gl.TEXTURE0);
   gl.bindTexture(gl.TEXTURE_2D, texture[backgroundTexture]);
-  gl.uniform1i(gl.getUniformLocation(glProgram, "sampler"), 0); 
+  gl.uniform1i(samplerUniform, 0);
+
+  gl.activeTexture(gl.TEXTURE1);
+  gl.bindTexture(gl.TEXTURE_2D, texture[2]);
+  gl.uniform1i(normalMapUniform, 1);
         
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer[backgroundObj]);
   loadIdentity();
@@ -735,6 +748,16 @@ function loadTextures() {
   image[1] = new Image();
   image[1].onload = function() { handleTextureLoaded(image[1], texture[1]); }
   image[1].src = "/resources/images/bgTex1.png";
+
+  texture[2] = gl.createTexture();
+  image[2] = new Image();
+  image[2].onload = function() { handleTextureLoaded(image[0], texture[0]); }
+  image[2].src = "/resources/images/noNormal.png";
+  
+  texture[3] = gl.createTexture();
+  image[3] = new Image();
+  image[3].onload = function() { handleTextureLoaded(image[1], texture[1]); }
+  image[3].src = "/resources/images/bgNormal.png";
 }
  
 function handleTextureLoaded(image, texture) {
@@ -750,6 +773,7 @@ function processObj(obj) {
     indices[obj]=[];
     normals[obj]=[];
     texCoords[obj]=[];
+    tangents[obj]=[];
 
     var highestIndex;
     var vertexIndex=[];
@@ -759,10 +783,22 @@ function processObj(obj) {
     var vertexData = [ ];
     var normalData = [ ];
     var texCoordData = [ ];
+    var tangentData = [];
+    var biTangentData = [];
     var facemap = { };
     var index = 0;
-    
+
+
         var linesForSwoop = text.split("\n");
+
+//*can be improved (doing it less times), but it shouldn't be too bad and gets the job done simply
+for (var v=0; v<linesForSwoop.length*3; v++)
+{
+    tangentData[v]=0;
+    biTangentData[v]=0;
+}
+   
+
         
         for (var lineIndex = 0; lineIndex<linesForSwoop.length; ++lineIndex) {
             var line = linesForSwoop[lineIndex];
@@ -776,7 +812,7 @@ function processObj(obj) {
             else if (value[0] == "vt") 
             {
                 texCoordData.push(parseFloat(value[1]));
-                texCoordData.push(-1*parseFloat(value[2])); //-1 because image is reversed on the Y
+                texCoordData.push(1-parseFloat(value[2])); //-1 because image is reversed on the Y
             }
             else if (value[0] == "vn") 
             {
@@ -786,46 +822,131 @@ function processObj(obj) {
             }
             else if (value[0] == "f") 
             {
-                for (var i = 1; i < 4; ++i) {
+ var vtx=[];
+var nor=[];
+var tex=[];
+value[3]=value[3].substr(0,value[3].length-1) //seems to be a line break character making it seem different
+
+                for (var i = 1; i < 4; i++) {
                     
                     if (!(value[i] in facemap)) {
                         // add a new entry to the map and arrays
                     var f = value[i].split("/");
-                    var vtx, nor, tex;
+                   
 
-                    vtx = parseInt(f[0]) - 1; //.obj starts vertices at 1, openGL starts at 0
-                    tex = parseInt(f[1]) - 1;
-                    nor = parseInt(f[2]) - 1;
+                    vtx[i-1] = parseInt(f[0]) - 1; //.obj starts vertices at 1, openGL starts at 0
+                    tex[i-1] = parseInt(f[1]) - 1;
+                    nor[i-1] = parseInt(f[2]) - 1;
 
-                    if (vtx * 3 + 2 < vertexData.length) 
+                    if (vtx[i-1] * 3 + 2 < vertexData.length) 
                     {
-                        vertices[obj].push(vertexData[vtx*3]);
-                        vertices[obj].push(vertexData[vtx*3+1]);
-                        vertices[obj].push(vertexData[vtx*3+2]);
+                        vertices[obj].push(vertexData[vtx[i-1]*3]);
+                        vertices[obj].push(vertexData[vtx[i-1]*3+1]);
+                        vertices[obj].push(vertexData[vtx[i-1]*3+2]);
                     }
                     
-                    if (nor * 3 + 2 < normalData.length) 
+                    if (nor[i-1] * 3 + 2 < normalData.length) 
                     {
-                        normals[obj].push(normalData[nor*3]);
-                        normals[obj].push(normalData[nor*3+1]);
-                        normals[obj].push(normalData[nor*3+2]);
+                        normals[obj].push(normalData[nor[i-1]*3]);
+                        normals[obj].push(normalData[nor[i-1]*3+1]);
+                        normals[obj].push(normalData[nor[i-1]*3+2]);
                     }
                     
 
-                    if (tex * 2 + 1 < texCoordData.length) {
-                        texCoords[obj].push(texCoordData[tex*2]);
-                        texCoords[obj].push(texCoordData[tex*2+1]);
+                    if (tex[i-1] * 2 + 1 < texCoordData.length) 
+                    {
+                        texCoords[obj].push(texCoordData[tex[i-1]*2]);
+                        texCoords[obj].push(texCoordData[tex[i-1]*2+1]);
                     }
-                    
 
                     facemap[value[i]] = index++;
+
                 }
+            else
+            {
+                    var f = value[i].split("/");
+                   
+
+                    vtx[i-1] = parseInt(f[0]) - 1; //.obj starts vertices at 1, openGL starts at 0
+                    tex[i-1] = parseInt(f[1]) - 1;
+                    nor[i-1] = parseInt(f[2]) - 1;
+            }
+
 
                 indices[obj].push(facemap[value[i]]);
                // currentGroup[1]++;
             }
+            var xDist1 = vertexData[vtx[1]*3]   - vertexData[vtx[0]*3];
+            var xDist2 = vertexData[vtx[2]*3]   - vertexData[vtx[0]*3];
+            var yDist1 = vertexData[vtx[1]*3+1] - vertexData[vtx[0]*3+1];
+            var yDist2 = vertexData[vtx[2]*3+1] - vertexData[vtx[0]*3+1];
+            var zDist1 = vertexData[vtx[1]*3+2] - vertexData[vtx[0]*3+2];
+            var zDist2 = vertexData[vtx[2]*3+2] - vertexData[vtx[0]*3+2];
+
+            var sDist1 = texCoordData[tex[1]*2] - texCoordData[tex[0]*2];
+            var sDist2 = texCoordData[tex[2]*2] - texCoordData[tex[0]*2];
+            var tDist1 = texCoordData[tex[1]*2+1] - texCoordData[tex[0]*2+1];
+            var tDist2 = texCoordData[tex[2]*2+1] - texCoordData[tex[0]*2+1];
+            var det = 1.0 / (sDist1 * tDist2 - sDist2 * tDist1);
+
+            var tanAdd = [];
+            var bTanAdd = [];
+
+
+             tanAdd[0] = (tDist2 * xDist1 - tDist1 * xDist2)*det;
+             tanAdd[1] = (tDist2 * yDist1 - tDist1 * yDist2)*det;
+             tanAdd[2] = (tDist2 * zDist1 - tDist1 * zDist2)*det;
+             bTanAdd[0] = (sDist1 * xDist2 - sDist2 * xDist1)*det;
+             bTanAdd[1] = (sDist1 * yDist2 - sDist2 * yDist1)*det;
+             bTanAdd[2] = (sDist1 * zDist2 - sDist2 * zDist1)*det;
+            
+            tangentData[3*vtx[0]]+=tanAdd[0];
+            tangentData[3*vtx[0]+1]+=tanAdd[1];
+            tangentData[3*vtx[0]+2]+=tanAdd[2];
+            tangentData[3*vtx[1]]+=tanAdd[0];
+            tangentData[3*vtx[1]+1]+=tanAdd[1];
+            tangentData[3*vtx[1]+2]+=tanAdd[2];
+            tangentData[3*vtx[2]]+=tanAdd[0];
+            tangentData[3*vtx[2]+1]+=tanAdd[1];
+            tangentData[3*vtx[2]+2]+=tanAdd[2];
+
+            biTangentData[3*vtx[0]]+=bTanAdd[0];
+            biTangentData[3*vtx[0]+1]+=bTanAdd[1];
+            biTangentData[3*vtx[0]+2]+=bTanAdd[2];
+            biTangentData[3*vtx[1]]+=bTanAdd[0];
+            biTangentData[3*vtx[1]+1]+=bTanAdd[1];
+            biTangentData[3*vtx[1]+2]+=bTanAdd[2];
+            biTangentData[3*vtx[2]]+=bTanAdd[0];
+            biTangentData[3*vtx[2]+1]+=bTanAdd[1];
+            biTangentData[3*vtx[2]+2]+=bTanAdd[2];
+
         }
         }
+
+for (var v=0; v<vertices[obj].length; v+=3)
+    {
+
+//to convert to tangents, which have 4 numbers in each segment, add v/3
+
+        tangents[obj][4*v/3]= tangentData[v] - normals[obj][v] * (normals[obj][v]*tangentData[v]+normals[obj][v+1]*tangentData[v+1]+normals[obj][v+2]*tangentData[v+2]);
+        tangents[obj][4*v/3+1]= tangentData[v+1] - normals[obj][v+1] * (normals[obj][v]*tangentData[v]+normals[obj][v+1]*tangentData[v+1]+normals[obj][v+2]*tangentData[v+2]);
+        tangents[obj][4*v/3+2]= tangentData[v+2] - normals[obj][v+2] * (normals[obj][v]*tangentData[v]+normals[obj][v+1]*tangentData[v+1]+normals[obj][v+2]*tangentData[v+2]);
+        var length = Math.sqrt(tangents[obj][4*v/3]*tangents[obj][4*v/3]+tangents[obj][4*v/3+1]*tangents[obj][4*v/3+1]+tangents[obj][4*v/3+2]*tangents[obj][4*v/3+2]);
+        tangents[obj][4*v/3]=tangents[obj][4*v/3]/length;
+        tangents[obj][4*v/3+1]=tangents[obj][4*v/3+1]/length;
+        tangents[obj][4*v/3+2]=tangents[obj][4*v/3+2]/length;
+
+        //normalize
+        if (((normals[obj][v+1]*tangents[obj][4*v/3+2] - normals[obj][v+2]*tangents[obj][4*v/3+1])*biTangentData[v] + (normals[obj][v+2]*tangents[obj][4*v/3] - normals[obj][v]*tangents[obj][4*v/3+2])*biTangentData[v+1] + (normals[obj][v]*tangents[obj][4*v/3+1] - normals[obj][v+1]*tangents[obj][4*v/3])*biTangentData[v+2])>0)
+        {
+            tangents[obj][4*v/3+3]=-1;
+        }
+        else
+        {
+            tangents[obj][4*v/3+3]=1;
+        }
+    }
+
 }
 
 
